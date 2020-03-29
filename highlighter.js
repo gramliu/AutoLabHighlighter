d466d776fa004cd697e4f33d2109d7a2eb2b1d8c
David Ethan Hwang <hdavidethan@gmail.com>
2020-03-29 13:54:52 -0400

    Added Animations

--- highlighter.js
+++ highlighter.js
@@ -6,6 +6,20 @@ let getOpenTag = (backgroundColor, colorText, id) => {
 };
 
 window.onload = () => {
+  let styles = document.createElement('style');
+  styles.innerHTML = `
+  @keyframes blinkingText{
+    0%{     color: #fff; }
+    49%{    color: #fff; }
+    60%{    color: black; }
+    99%{    color: black; }
+    100%{   color: #fff; }
+  }
+  span.blinking {
+    animation: blinkingText 1s linear 1s infinite normal;
+  }`;
+  document.head.appendChild(styles);
+
   let elems = document.body.getElementsByTagName("pre");
   let content = elems[0].innerHTML;
   let lines = content.split("\n");
@@ -15,9 +29,15 @@ window.onload = () => {
   let id = 0;
   for (let i = 0; i < lines.length; i++) {
     if (lines[i].startsWith("*** TEST FAILED!")) {
+      let lines_text = lines[i - 1];
       lines[i - 1] = getOpenTag("#f44336", "white", id++) + lines[i - 1] + closeTag;
-      let tokens = lines[i-1].split(" ");
-      let text = tokens[1] + " " + tokens[2];
+      let tokens = lines_text.split(" ");
+      let text;
+      if (tokens[1].includes(',')) {
+        text = tokens[1].slice(0,-1);
+      } else {
+        text = tokens[1] + " " + tokens[2].slice(0, -1);
+      }
       errors.push({
           id: `task-${id}`,
           text: text
@@ -39,8 +59,10 @@ window.onload = () => {
     }
   }
   elems[0].innerHTML = lines.join("\n");
-
-  chrome.runtime.sendMessage({
-      data: errors
-    }, (response) => {});
+  
+  chrome.runtime.onMessage.addListener((message, sender, response) => {
+    if (message.greeting == "hello") {
+      response({"errors": errors})
+    }
+  });
 };
--- popup.js
+++ popup.js
@@ -1,7 +1,33 @@
-chrome.runtime.onMessage.addListener((message, sender, response) => {
-    console.log(sender.tab
-        ? "from a content script:" + sender.tab.url
-        : "from the extension"
-    );
-    console.log(message.errors);
+function hello() {
+  console.log("elem")
+}
+
+window.addEventListener('load', () => {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+      console.log(response.errors);
+      let error_div = document.getElementById('errors');
+      response.errors.forEach(error => {
+        let par = document.createElement('p');
+        par.id = error.id;
+        par.className = 'clickable';
+        par.innerHTML = error.text;
+        error_div.appendChild(par);
+      });
+      let selection = document.getElementsByClassName('clickable');
+      for (let i = 0; i < selection.length; i++) {
+        selection[i].addEventListener("click", function() {
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.executeScript(tabs[0].id, {
+              "code": "var id=" + i + "; var blinking; var elem;"
+            }, () => {
+              chrome.tabs.executeScript(tabs[0].id, {
+                "code": "blinking = document.getElementsByClassName('blinking'); for (let i=0; i < blinking.length; i++) {blinking[i].classList.remove('blinking')} elem = document.getElementById(id); elem.scrollIntoView({behavior:'smooth'}); elem.classList.add('blinking');"
+              })
+            })
+          })
+        })
+      }
+    });
   });
+});
