95fd8b118b51c33698e30bcfbf6c1aa94be8b1e0
Gram Liu <gram.liu226@gmail.com>
2020-03-29 01:07:54 -0400

    Added simple highlighting

--- highlighter.js
+++ highlighter.js
@@ -1,4 +1,12 @@
 window.onload = () => {
     let elems = document.body.getElementsByTagName("pre");
     let content = elems[0].innerHTML;
+    let lines = content.split("\n");
+    let openTag = "<span style='background-color: yellow'>";
+    for (let i = 0; i < lines.length; i++) {
+        if (lines[i].startsWith("*** TEST FAILED!")) {
+            lines[i-1] = openTag + lines[i-1] + "</span>";
+        }
+    }
+    elems[0].innerHTML = lines.join("\n");
 }
